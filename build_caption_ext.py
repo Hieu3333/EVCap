9601fee42b4e76bb2bb0234d2fd40c103fbbb8a9
H <hieu30029081@gmail.com>
2024-12-11 16:38:46 +0700

    fix batch bug

--- build_caption_ext.py
+++ build_caption_ext.py
@@ -31,7 +31,7 @@ def preprocess_image(img_path):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
     ])
-    return transform(img).unsqueeze(0)
+    return transform(img)
 
 def load_model(ckpt_path, device, model_type="lmsys/vicuna-13b-v1.3"):
     model = EVCap(
