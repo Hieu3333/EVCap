ac11c9b508209b56b09c108ef9d59b556cf02e5f
H <hieu30029081@gmail.com>
2024-12-12 01:11:57 +0700

    a

--- check_ext.py
+++ check_ext.py
@@ -1,9 +1,7 @@
 import pickle
 with open('ext_data/caption_ext_memory.pkl','rb') as f:
-  data = pickle.load(f)
+  img_feature = pickle.load(f)
   print('loaded data')
-  img_feature = data["image_features"]
-  captions = data["captions"]
-  print(img_feature.shape,len(captions))
+  
+  print(img_feature.shape)
   print(img_feature[:5])
-  print(captions[:5])
