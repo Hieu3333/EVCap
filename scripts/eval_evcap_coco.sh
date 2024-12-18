38bcb36ba4f700a6b1fb28330105c0c481f32b55
H <hieu30029081@gmail.com>
2024-12-18 13:19:03 +0700

    a

--- scripts/eval_evcap_coco.sh
+++ scripts/eval_evcap_coco.sh
@@ -3,21 +3,21 @@ SHELL_FOLDER=/workspace/EVCap/scripts
 cd $SHELL_FOLDER/..
 EXP_NAME=$1
 DEVICE=$2
-NOCAPS_OUT_PATH=results/$EXP_NAME
+COCO_OUT_PATH=results/$EXP_NAME
 
 TIME_START=$(date "+%Y-%m-%d-%H-%M-%S")
 LOG_FOLDER=logs/${EXP_NAME}_EVAL
 mkdir -p $LOG_FOLDER
 
-NOCAPS_LOG_FILE="$LOG_FOLDER/COCO_${TIME_START}.log"
+COCO_LOG_FILE="$LOG_FOLDER/COCO_${TIME_START}.log"
 
 python -u eval_evcap.py \
 --device cuda:$DEVICE \
 --name_of_datasets coco \
 --path_of_val_datasets sampled_annotations.json \
 --image_folder annotations/coco/val2014/val2014/ \
---out_path=$NOCAPS_OUT_PATH \
-|& tee -a  ${NOCAPS_LOG_FILE}
+--out_path=$COCO_OUT_PATH \
+|& tee -a  ${COCO_LOG_FILE}
 
 echo "==========================COCO EVAL================================"
-python evaluation/cocoeval.py --result_file_path $NOCAPS_OUT_PATH/coco*.json |& tee -a  ${NOCAPS_LOG_FILE}
+python evaluation/cocoeval.py --result_file_path $COCO_OUT_PATH/coco*.json |& tee -a  ${COCO_LOG_FILE}
