ff8b117ce613d1abc11deaeca3bee0b372ff92dd
H <Hieu.NH215202@sis.hust.edu.vn>
2025-05-13 14:14:56 +0700

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-# Exit the script on error
+
 set -e
 cd /workspace
-# Step 1: Install Miniconda
 echo "Installing Miniconda..."
-# Download Miniconda (Linux version)
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-# Install Miniconda silently
 bash miniconda.sh -b -p $HOME/miniconda
-# Initialize conda (adds it to the PATH)
 $HOME/miniconda/bin/conda init
-# Reload shell
 source ~/.bashrc
 
-# Step 2: Clone the remote repository to /workspace
+
 echo "Cloning the repository..."
-# Change to the workspace directory (ensure you have access to it)
-git clone https://github.com/Hieu3333/EVCap.git
+git clone https://github.com/NHH-123/EVCap.git
 cd EVCap
 
-# Step 3: Create a conda environment from environment.yaml inside /workspace
 echo "Creating conda environment from environment.yaml..."
-# Create the environment inside /workspace
 conda env create -f environment.yaml -p /workspace/env_evcap
-# Activate the environment
+
 source activate /workspace/env_evcap
 
 echo "Environment setup complete."
@@ -44,4 +36,6 @@ conda install -c conda-forge google-cloud-storage
 echo "Downloading Coco dataset"
 wget http://images.cocodataset.org/zips/train2014.zip
 unzip -q train2014.zip -d data/coco/coco2014
+
+
 
